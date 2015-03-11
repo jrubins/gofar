3c38652d016316d16ab8460e1cb5c73f8a0dc9e2
Jonathan Rubins <jonrubins@gmail.com>
2015-03-10 21:37:57 -0500

    HELP TEXT
    Added help text for the checkbox components as DOM "title" attributes.
    Got the definitions from Table 1 of the article. Added Bootstrap fonts
    to use glyphicons so I can display a "info" icon on mobile with the help
    text eventually.

--- js/app/gofar.js
+++ js/app/gofar.js
@@ -6,10 +6,13 @@
 	 * @param {String} label - The label for the component.
 	 * @param {int} points - The number of points to add to the total when the component
 	 * 		is selected.
+	 * @param {String} helpText - The help text that defines the component to display as
+	 * 		the HTML "title" attribute to the user.
 	 */
-	GO_FAR.Component = function(label, points) {
+	GO_FAR.Component = function(label, points, helpText) {
 		this.label = label;
 		this.points = points;
+		this.helpText = helpText;
 
 		// this is whether or not the component is currently selected, all components
 		// start out as not selected
@@ -31,47 +34,57 @@
 		this.staticComponents = [
 			{
 				label: "Moderate or Severe cognitive/neurologic disability*",
-				points: 15			
+				points: 15,
+				helpText: "Moderate = able to live independently with disabilities which may include hemiplegia, seizures, ataxia, dysphagia, or permanent memory or mental changes"
 			},
 			{
 				label: "Admission from a skilled nursing facility",
-				points: 6			
-			},
-			{
-				label: "Metastatic or hematologic cancer",
-				points: 7			
-			},
-			{
-				label: "Major trauma",
-				points: 10			
-			},
-			{
-				label: "Pneumonia",
-				points: 1			
-			},
-			{
-				label: "Septicemia",
-				points: 7
-			},
-			{
-				label: "Hypotension or hypoperfusion",
-				points: 5
-			},
-			{
-				label: "Acute stroke",
-				points: 8
-			},
-			{
-				label: "Respiratory insufficiency",
-				points: 4
-			},
-			{
-				label: "Hepatic insufficiency",
 				points: 6
 			},
 			{
+				label: "Metastatic or hematologic cancer",
+				points: 7,
+				helpText: "Any solid tissue malignancy with evidence of metastasis or any blood-borne malignancy"
+			},
+			{
+				label: "Major trauma",
+				points: 10,
+				helpText: "Evidence of multisystem injury or single-system injury associated with shock or altered mental status during the current hospitalization"
+			},
+			{
+				label: "Pneumonia",
+				points: 1,
+				helpText: "Documented diagnosis of active pneumonia, in which antibiotic therapy has not yet been started or is still ongoing"
+			},
+			{
+				label: "Septicemia",
+				points: 7,
+				helpText: "Documented bloodstream infection in which anti-biotic therapy has not yet been started or is still ongoing"
+			},
+			{
+				label: "Hypotension or hypoperfusion",
+				points: 5,
+				helpText: "Any evidence of hypotension within 4 h of the event, defined as any of the following: SBP <90 or MAP <60 mm Hg; vasopressor or inotropic requirement after volume expansion (except for dopamine <= 3 microgram/kg/min); or intra-aortic balloon pump"
+			},
+			{
+				label: "Acute stroke",
+				points: 8,
+				helpText: "Documented diagnosis of an intracranial or intra-ventricular hemorrhage or thrombosis during the current admission"
+			},
+			{
+				label: "Respiratory insufficiency",
+				points: 4,
+				helpText: "Evidence of acute or chronic respiratory insufficiency within 4 h of the event, defined as any of the following: PaO2/FiO2 ratio <300, PaO2 <60 mm Hg, or SaO2 <90% (without preexisting cyanotic heartdisease); PaCO2, ETCO2, or TcCO2 >50 mm Hg; spontaneous respiratory rate >40/min or <5/min; requirement for noninvasive ventilation (eg, bag-valve mask, mask CPAP or BiPAP, nasal CPAP or BiPAP), or negative pressure ventilation; or requirement for ventilation via invasive airway"
+			},
+			{
+				label: "Hepatic insufficiency",
+				points: 6,
+				helpText: "Evidence of hepatic insufficiency within 24 h of the event, defined by total bilirubin >2 mg/dL (to convert to micromoles per liter, multiply by 17.104) and AST > 2 times the upper limit of normal or cirrhosis"
+			},
+			{
 				label: "Renal insufficiency or dialysis",
-				points: 4
+				points: 4,
+				helpText: "Requiring ongoing dialysis or extracorporeal filtration therapies, or serum creatinine >2 mg/dL (to convert to micromoles per liter, multiply by 88.4) within 24 h of the event"
 			},
 			{
 				label: "Medical noncardiac diagnosis",
@@ -207,7 +220,7 @@
 		// loop over each of our static components so we can build up our components objects
 		_.each(this.staticComponents, function(component) {
 			// add a new component to our array for each static one we have
-			this.components.push(new GO_FAR.Component(component.label, component.points));
+			this.components.push(new GO_FAR.Component(component.label, component.points, component.helpText));
 		}, this);
 
 		return this;
