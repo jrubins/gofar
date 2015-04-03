f28eb621862793ac197d7228323a471b54de3b09
Jonathan Rubins <jonrubins@gmail.com>
2015-04-03 16:50:52 -0500

    STYLE
    Updated comment style and added some comments.

--- js/app/gofar.js
+++ js/app/gofar.js
@@ -14,8 +14,8 @@
 		this.points = points;
 		this.helpText = helpText;
 
-		// this is whether or not the component is currently selected, all components
-		// start out as not selected
+		// This is whether or not the component is currently selected. All components
+		// start out as not selected.
 		this.selected = ko.observable(false);
 	};
 
@@ -26,11 +26,11 @@
 	GO_FAR.ViewModel = function() {
 		var self = this;
 
-		// the initial points that the calculator should start at
+		// The initial points that the calculator should start at.
 		var initialPoints = -15;
 
-		// all the static components we'll display as checkboxes for the user to select for
-		// the calculation
+		// All the static components we'll display as checkboxes for the user to select for
+		// the calculation.
 		this.staticComponents = [
 			{
 				label: "Moderate or Severe cognitive/neurologic disability",
@@ -92,21 +92,21 @@
 			}
 		];
 
-		// true if any of the components are currently selected, false otherwise 
+		// True if any of the components are currently selected, false otherwise.
 		this.anySelected = ko.observable(false);
 
-		// the total number of points from adding up all the selected components currently
+		// The total number of points from adding up all the currently selected components.
 		this.componentPoints = ko.observable(initialPoints);
 
-		// the total number of points for the currently entered age group
+		// The total number of points for the currently entered age group.
 		this.agePoints = ko.observable(0);
 
-		// the total number of points - combination of the component points and the age points
+		// The total number of points. This is a combination of the component points and the age points.
 		this.totalPoints = ko.computed(function() {
 			return self.componentPoints() + self.agePoints();
 		});
 
-		// the probability for the calculation, depending on the total number of points
+		// The probability for the calculation, depending on the total number of points.
 		this.probability = ko.computed(function() {
 			if(self.totalPoints() >= 24) {
 				return "0.9%";
@@ -124,21 +124,21 @@
 		 * @param {Object} component - The component that has either been selected or de-selected.
 		 */
 		this.addComponentPoints = function(component) {
-			// we want to either add or subtract points depending on whether or not the component
-			// is now selected or de-selected
+			// We want to either add or subtract points depending on whether or not the component
+			// is now selected or de-selected.
 			var newPoints = component.selected() ? -1 * component.points : component.points;
 			
-			// add our new points to our point total so far
+			// Add our new points to our point total so far.
 			self.componentPoints(self.componentPoints() + newPoints);
 
-			// our component is now the opposite of its previously selected state
+			// Our component is now the opposite of its previously selected state.
 			component.selected(!component.selected());
 
-			// update whether or not we have any components or age value selected / entered
+			// Update whether or not we have any components or age value selected / entered.
 			self.updateAnySelected();
 
-			// return true to perform the default behavior - otherwise our checkbox will not get
-			// checked
+			// Return true to perform the default behavior - otherwise our checkbox will not get
+			// checked.
 			return true;
 		};
 
@@ -148,13 +148,13 @@
 		 * the "Clear All" button.
 		 */
 		this.updateAnySelected = function() {
-			// find any component that is currently selected
+			// Find any component that is currently selected.
 			var selectedComponent = _.find(self.components(), function(component) {
 				return component.selected();
 			});
 
-			// check if we have any components selected now or if we have a value for the
-			// age of the patient
+			// Check if we have any components selected now or if we have a value for the
+			// age of the patient.
 			if(selectedComponent || self.patientAge()) {
 				self.anySelected(true);
 			} else {
@@ -167,36 +167,36 @@
 		 * back to zero.
 		 */
 		this.clearAll = function() {
-			// loop over all of our components and mark them as no longer checked
+			// Loop over all of our components and mark them as no longer checked.
 			_.each(self.components(), function(component) {
 				component.selected(false);
 			});
 
-			// reset our component point total back to zero
+			// Reset our component point total back to its initial value.
 			self.componentPoints(initialPoints);
 
-			// get rid of any value that was entered for the patient age.
+			// Get rid of any value that was entered for the patient age.
 			self.patientAge("");
 
-			// reset our age points total back to zero
+			// Reset our age points total back to zero.
 			self.agePoints(0);
 
-			// we no longer have any more checked components
+			// We no longer have any more checked components.
 			self.anySelected(false);
 		};
 
-		// an array of components that we'll allow the user to interact with to create their calculation
+		// An array of components that we'll allow the user to interact with to create their calculation.
 		this.components = ko.observableArray([]);
 
-		// the patient age input value
+		// The patient age input value.
 		this.patientAge = ko.observable();
 
-		// subscribe to when a user enters a value for the patient's age
+		// Subscribe to when a user enters a value for the patient's age.
 		this.patientAge.subscribe(function(newPatientAge) {
-			// attempt to parse our entered age as a number
+			// Attempt to parse our entered age as a number.
 			var parsedAge = parseInt(newPatientAge, 10);
 
-			// check if the entered number could be parsed as a number or not
+			// Check if the entered number could be parsed as a number or not.
 			if(!isNaN(parsedAge)) {
 				if(newPatientAge >= 70 && newPatientAge < 75) {
 					self.agePoints(2);
@@ -206,20 +206,20 @@
 					self.agePoints(6);
 				} else if(newPatientAge >= 85) {
 					self.agePoints(11);
-				} else { // if the user types any other number range
+				} else { // If the user types any other number range.
 					self.agePoints(0);
 				}
-			} else { // the user typed something that can't be a number
+			} else { // The user typed something that can't be a number.
 				self.agePoints(0);
 			}
 
-			// update whether or not we have any components or age value entered
+			// Update whether or not we have any components or age value entered.
 			self.updateAnySelected();
 		});
 
-		// loop over each of our static components so we can build up our components objects
+		// Loop over each of our static components so we can build up our components objects.
 		_.each(this.staticComponents, function(component) {
-			// add a new component to our array for each static one we have
+			// Add a new component to our array for each static one we have.
 			this.components.push(new GO_FAR.Component(component.label, component.points, component.helpText));
 		}, this);
 
@@ -227,7 +227,7 @@
 	};
 
 	$(document).on('ready', function() {
-		// bind our model to our view
+		// Bind our view model to our view.
 		ko.applyBindings(new GO_FAR.ViewModel(), $("#content")[0]);
 
 		// Initialize our Bootstrap tooltips.
@@ -239,24 +239,26 @@
 			}
 		});
 
-		// add our hover listener so we can change the background color to inform
-		// the user they can select an option
+		// Add our hover listener so we can change the background color to inform
+		// the user they can select an option.
 		$("#components .component").hover(function() {
 			$(this).addClass("active");
 		}, function() {
 			$(".component").removeClass("active");
 		});
 
-		// bind to user keypresses on the patient age input box to prevent
-		// submitting the form if a user clicks enter
+		// Bind to user keypresses on the patient age input box to prevent
+		// submitting the form if a user clicks the 'Enter' key.
 		$("#patientAge").on('keydown', function(event) {
-			// check if the key pressed was the enter key
+			// Check if the key pressed was the enter key.
 			if(event.keyCode === 13) {
 				event.preventDefault();
 				return false;
 			}
 		});
 
+		// Bind to a user clicking a component help tooltip - we want to prevent
+		// the default action here of selecting the component.
 		$(".component-help-text").on('click', function(event) {
 			event.preventDefault();
 			return false;
