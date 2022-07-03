3e009106b4be5e9b805421efb5fc3ca8f4f83de0
Jonathan Rubins <jonrubins@gmail.com>
2022-07-02 20:44:21 -0500

    Add cssnano plugin during production

--- postcss.config.js
+++ postcss.config.js
@@ -4,5 +4,10 @@ module.exports = {
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env'),
     require('autoprefixer'),
+    process.env.NODE_ENV === 'production'
+      ? require('cssnano')({
+          preset: 'default',
+        })
+      : false,
   ],
 }
